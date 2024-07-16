879a4db36b6f67f8ccba4cf38992768a25b4adb6
Subhransu Jena <subunc5@gmail.com>
2024-07-16 08:12:40 +0530

    handle uncaught exceptions & unhandled rejection

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
+process.on('uncaughtException', (err) => {
+  console.log(`UNCAUGHT EXCEPTION`);
+  console.log(err.name, ' : ', err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE;
 mongoose.connect(DB).then((con) => {
   // console.log(con.connections);
@@ -12,6 +18,14 @@ mongoose.connect(DB).then((con) => {
 
 // console.log(process.env);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`SERVER RUNNING ON DA ${port} ayayaya`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(`UNHANDLED REJECTION`);
+  console.log(err.name, ' : ', err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
