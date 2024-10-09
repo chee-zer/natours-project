ca77991fbaa3f92c6bf45422c01c256c86fb0db0
Subhransu Jena <subunc5@gmail.com>
2024-10-09 18:21:40 +0530

    data sanitization, helmet, xss, hpp, rate limiter

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const { default: helmet } = require('helmet');
+const hpp = require('hpp');
 
 const app = express();
 const port = 3000;
 
 //Middlewares
 
+app.use(helmet());
+
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  max: 3,
+  windowMs: 3600000,
+  message: 'Too many messages from this IP! Please try again later.',
+});
+
+app.use('/api', limiter);
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
 app.use(express.json()); //move to routers?
 app.use(express.static(`${__dirname}/public`));
 
--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,17 @@ const signToken = (id) => {
 const sendToken = (user, statusCode, res) => {
   const token = signToken(user.id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 3600000,
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
   res.status(statusCode).json({
     status: 'success',
     token,
